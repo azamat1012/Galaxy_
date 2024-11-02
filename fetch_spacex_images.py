d638601e508d471e4774a6bb12eb0bc1438e393e
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2024-11-02 17:40:32 +0500

    Update fetch_spacex_images.py

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -10,8 +10,8 @@ images_path = f"{current_dir}/images/spaceX/"
 def fetch_spacex_last_launch(launch_id=None, place_to_save=images_path):
     spaceX_url = f"https://api.spacexdata.com/v5/launches/{launch_id}" if launch_id else "https://api.spacexdata.com/v5/launches/latest"
     spaceX_response = requests.get(spaceX_url)
-    text = spaceX_response.json()
-    flickr_images = text['links']['flickr']['original']
+    images_data = spaceX_response.json()
+    flickr_images = images_data['links']['flickr']['original']
 
     for index, url in enumerate(flickr_images, start=1):
         img_name = f"spacex_{index}"
