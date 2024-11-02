4c1cb99ac2ece81d03ce25a0ef48b6a60cfa2c7f
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2024-11-02 17:25:05 +0500

    Update fetch_nasa_apod.py

--- fetch_nasa_apod.py
+++ fetch_nasa_apod.py
@@ -8,19 +8,15 @@ images_path = f"{current_dir}/images/spaceX/"
 
 
 def fetch_nasa_apod(api_key, place_to_save=images_path):
-
     params = {
         "api_key": api_key,
         "count": 30,
     }
-
     nasa_url = f"https://api.nasa.gov/planetary/apod"
-
     response = requests.get(nasa_url, params=params)
     response.raise_for_status()
 
     url_to_pic = response.json()
-
     for index, item in enumerate(url_to_pic, start=1):
         url = item.get("url")
         if url:
@@ -28,8 +24,11 @@ def fetch_nasa_apod(api_key, place_to_save=images_path):
             load_image(url, place_to_save, name_of_img=file_name)
 
 
-
-if __name__ == "__main__":
+def main():
     load_dotenv(".env")
     API_KEY_NASA = os.getenv("API_KEY_NASA")
     fetch_nasa_apod(API_KEY_NASA)
+
+
+if __name__ == "__main__":
+    main()
