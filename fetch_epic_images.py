17b600de5acfc8a633f9f16e8644350b9c687c52
azamat1012 <148556590+azamat1012@users.noreply.github.com>
2024-11-04 12:13:24 +0500

    Update fetch_epic_images.py

--- fetch_epic_images.py
+++ fetch_epic_images.py
@@ -3,12 +3,7 @@ import requests
 from dotenv import load_dotenv
 from load_image import load_image
 
-load_dotenv(".env")
-current_dir = os.path.dirname(__file__)
-images_path = f"{current_dir}/images/spaceX/"
-API_KEY_NASA = os.getenv("API_KEY_NASA")
-
-def fetch_nasa_epic(api_key, place_to_save=images_path):
+def fetch_nasa_epic(api_key, place_to_save):
     url = "https://api.nasa.gov/EPIC/api/natural/images"
     params = {"api_key": api_key}
     response = requests.get(url, params=params)
@@ -21,10 +16,13 @@ def fetch_nasa_epic(api_key, place_to_save=images_path):
         img_url = f"{base_url}/{formatted_time}/png/{img_name}.png"
         load_image(img_url, place_to_save, name_of_img=img_name)
 
-
-def main():
-    fetch_nasa_epic(API_KEY_NASA)
-
-
 if __name__ == "__main__":
-    main()
+    load_dotenv(".env")
+    current_dir = os.path.dirname(__file__)
+    images_path = f"{current_dir}/images/spaceX/"
+    API_KEY_NASA = os.getenv("API_KEY_NASA")
+
+    if API_KEY_NASA:
+        fetch_nasa_epic(API_KEY_NASA, images_path)
+    else:
+        print("API_KEY_NASA is missing in environment variables.")
